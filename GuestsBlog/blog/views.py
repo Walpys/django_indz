48f1a45dfc8e295364c17ba6070bf2cb04975be5
Walpys <artemkrat073@gmail.com>
2026-04-20 13:49:43 +0300

    create method layouts in views, implement paths in urls.py

--- GuestsBlog/blog/views.py
+++ GuestsBlog/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def get_post_list(request):
+    return HttpResponse("<h1>Список постів</h1><p>Тут буде перелік усіх записів.</p>")
+
+def get_post_detail(request, post_id):
+    return HttpResponse(f"<h1>Перегляд поста №{post_id}</h1><p>Зміст окремого поста.</p>")
+
+def post_create(request):
+    return HttpResponse("<h1>Створення нового поста</h1><form>Форма буде тут.</form>")
+
+def add_comment(request, post_id):
+    return HttpResponse(f"<h1>Додавання коментаря</h1><p>Ви коментуєте пост №{post_id}.</p>")
